21dc6980f22319543332f78cd8cd77e80c0cc9d0
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-07-11 12:09:39 +0200

    Update version for the next release (v.1.5)

--- functions/lib/meilisearch/create-index.js
+++ functions/lib/meilisearch/create-index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initMeilisearchIndex = void 0;
 const meilisearch_1 = require("meilisearch");
+const agents_1 = require("./agents");
 /**
  * initMeilisearchIndex
  * @param {MeilisearchConfig} - Meilisearch configuration
@@ -11,6 +12,7 @@ function initMeilisearchIndex({ host, apiKey, indexUid, }) {
     const client = new meilisearch_1.MeiliSearch({
         host,
         apiKey,
+        clientAgents: (0, agents_1.constructClientAgents)(),
     });
     return client.index(indexUid);
 }
