06db93e14757fef3d0c8ca2c7008122a02878e4b
oplik0 <opliko.reg@protonmail.com>
2023-02-18 15:42:09 +0100

    fix eslint errors on firebase-admin imports
    the import plugin doesn't support `exports` property in package.json so it doesn't see newer firebase-admin exports. eslint-import-resolver-typescript fixes this

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -51,4 +51,15 @@ module.exports = {
       },
     ],
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: './tsconfig.json',
+      },
+    },
+  },
 }
